de48c2eac3ea03c52acbe6e78632210aaf5c5c43
zefetorres <zefetorres@icloud.com>
2021-06-09 13:19:47 -0500

    add strucure and styles to checkout component

--- src/views/CheckoutView.jsx
+++ src/views/CheckoutView.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import '../assets/styles/views/checkout-view.css';
 
 export const CheckoutView = () => {
     return (
-        <div>
+        <div className="checkout">
             <h1>Checkout view</h1>
+            <div className="checkout__products">
+                <div className="row">
+                    <div className="checkout__product">                    
+                        <p>IMAGEN</p>
+                        <h2>Bocina JBL Go 2 portátil con bluetooth deep sea blue</h2>
+                        <p>1</p>
+                        <h2>$4,565.00</h2>
+                        <span className="ti-trash"></span>
+                    </div>
+                    <div className="checkout__product">                    
+                        <p>IMAGEN</p>
+                        <h2>Bocina JBL Go 2 portátil con bluetooth deep sea blue</h2>
+                        <p>1</p>
+                        <h2>$4,565.00</h2>
+                        <span className="ti-trash"></span>
+                    </div>
+                    <div className="checkout__product">                    
+                        <p>IMAGEN</p>
+                        <h2>Bocina JBL Go 2 portátil con bluetooth deep sea blue</h2>
+                        <p>1</p>
+                        <h2>$4,565.00</h2>
+                        <span className="ti-trash"></span>
+                    </div>
+                </div>
+                <div className="checkout-amount">
+                    <h2>Total: $8,989.00</h2>
+                    <button>Continue shopping</button>
+                </div>
+            </div>
         </div>
     )
 }
